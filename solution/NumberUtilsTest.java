36c5f7a2a12f0b4435a4888b6484ce27816ed22b
JulianaGSouza <juulianags@gmail.com>
2023-08-30 15:12:16 -0400

    Init Repo

---
+++ solution/NumberUtilsTest.java
@@ -0,0 +1,15 @@
+package delft;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class NumberUtilsTest {
+
+}
